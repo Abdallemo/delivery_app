15f8c42b20946935b757d73950e31f081a3e1dd0
EngCamo <learn3038it@gmail.com>
2024-12-14 07:02:17 +0800

    auth

--- lib/services/Auth/auth_gate.dart
+++ lib/services/Auth/auth_gate.dart
@@ -1,8 +1,6 @@
 import 'package:deliver/pages/home_page.dart';
-import 'package:deliver/pages/login_page.dart';
 import 'package:deliver/services/Auth/login_or_regsiter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
