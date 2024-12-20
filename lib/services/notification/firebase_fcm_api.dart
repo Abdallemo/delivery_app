3a1f35860bb71b77264fdf9259b9c7e0106c7210
EngCamo <learn3038it@gmail.com>
2024-12-21 06:26:51 +0800

    done the notification part

--- lib/services/notification/firebase_fcm_api.dart
+++ lib/services/notification/firebase_fcm_api.dart
@@ -1,12 +1,22 @@
+import 'package:deliver/components/my_notification.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class FirebaseFcmApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
-
-  Future<void> initNotifications()async{
+  Future<void> initNotifications() async {
     await _firebaseMessaging.requestPermission();
 
     final FCMToken = await _firebaseMessaging.getToken();
+    await _firebaseMessaging.subscribeToTopic('all_users');
+    FirebaseMessaging.onMessage.listen((message) {
+      final notification = message.notification;
+      if (notification == null) return;
+      NotificationHelper.showNotification(
+          id: notification.hashCode,
+          title: notification.title.toString(),
+          body: notification.body.toString());
+    });
 
     print('Token:$FCMToken');
   }
